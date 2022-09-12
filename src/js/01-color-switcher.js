673684d16e1d1e1ac91053e52d3228a1a0f44a77
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-12 22:43:28 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const COLORCHANGE_DELAY = 1000;
+let timeIntervalId = null;
 
+startEl.addEventListener('click', () => {
+  timeIntervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, COLORCHANGE_DELAY);
+  startEl.disabled = true;
+  stopEl.disabled = false;
+});
+
+stopEl.addEventListener('click', () => {
+  clearInterval(timeIntervalId);
+  startEl.disabled = false;
+  stopEl.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
