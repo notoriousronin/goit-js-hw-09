2d944c27c2cc4fdee26c322da82875c389b3e1be
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-13 00:36:36 +0300

    2

---
+++ src/js/02-timer.js
@@ -0,0 +1,27 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const startEl = document.querySelector('[data-start]');
+const dateTime = document.querySelector('#datetime-picker');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+const initialTime = null;
+
+const timer = {};
+
+startEl.addEventListener('click', () => {
+  timer.start();
+});
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    initialTime = selectedDates[0].getTime();
+  },
+};
